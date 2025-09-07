201cc9943f83ac135960d44888afb7138d958d8d
edinstance <edinstance@gmail.com>
2025-09-07 10:45:46 +0100

    refactor: refactored health check to be in resolvers directory

--- services/flights/internal/server/routes.go
+++ services/flights/internal/server/routes.go
@@ -2,13 +2,15 @@ package server
 
 import (
 	"net/http"
+
+	"github.com/edinstance/distributed-aviation-system/services/flights/internal/resolvers/health"
 )
 
 func NewMux() *http.ServeMux {
 	mux := http.NewServeMux()
-	
+
 	// Health check
-	mux.HandleFunc("/health", HealthHandler)
+	mux.HandleFunc("/health", health.HealthHandler)
 
 	return mux
 }
