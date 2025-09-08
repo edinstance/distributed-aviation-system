0c635d62459e7fa33c2ff760fc93ce3f525b9601
edinstance <edinstance@gmail.com>
2025-09-08 18:41:23 +0100

    feat: added .env file support and fixed logging

--- services/flights/cmd/main.go
+++ services/flights/cmd/main.go
@@ -2,16 +2,23 @@ package main
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/edinstance/distributed-aviation-system/services/flights/internal/logger"
 	"github.com/edinstance/distributed-aviation-system/services/flights/internal/server"
+	"github.com/joho/godotenv"
 	"golang.org/x/net/http2"
 	"golang.org/x/net/http2/h2c"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("No .env file found, relying on environment variables")
+	}
 
 	port := os.Getenv("PORT")
 	env := os.Getenv("ENVIRONMENT")
@@ -26,6 +33,6 @@ func main() {
 
 	logger.Info("FlightsService listening", "addr", addr)
 	if err := http.ListenAndServe(addr, h2c.NewHandler(mux, &http2.Server{})); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		logger.Error("Server error: %v", err)
+		logger.Error("Server error", "err", err)
 	}
 }
