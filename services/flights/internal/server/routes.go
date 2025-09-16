0af3e5ee90dff59c23011faf6d41f752b49fabe8
edinstance <edinstance@gmail.com>
2025-09-17 00:14:48 +0100

    feat!: refactored app config and only configured playgropund to be unavailable in prod

--- services/flights/internal/server/routes.go
+++ services/flights/internal/server/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/edinstance/distributed-aviation-system/services/flights/internal/config"
 	v1connect "github.com/edinstance/distributed-aviation-system/services/flights/internal/protobuf/flights/v1/flightsv1connect"
 	"github.com/edinstance/distributed-aviation-system/services/flights/internal/resolvers/health"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -19,7 +20,10 @@ func NewMux(pool *pgxpool.Pool) *http.ServeMux {
 
 	// GraphQL handlers
 	mux.Handle("/graphql", newGraphQLHandler(pool))
-	mux.Handle("/playground", playground.Handler("GraphQL Playground", "/graphql"))
+
+	if config.App.Environment != "prod" {
+		mux.Handle("/playground", playground.Handler("GraphQL Playground", "/graphql"))
+	}
 
 	// Health check
 	mux.HandleFunc("/health", health.HealthHandler)
