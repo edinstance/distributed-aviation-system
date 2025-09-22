b3e57edd5e60e7b8526c0604c4e06b417af4a0e0
edinstance <edinstance@gmail.com>
2025-09-22 10:44:32 +0100

    fix: updated go healthcheck to follow same standard as router

--- services/flights/internal/resolvers/health/health.go
+++ services/flights/internal/resolvers/health/health.go
@@ -11,5 +11,5 @@ func HealthHandler(w http.ResponseWriter, _ *http.Request) {
 
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	_, _ = w.Write([]byte(`{"status":"ok"}`))
+	_, _ = w.Write([]byte(`{"status":"UP"}`))
 }
