d792eeb70da7b9e19d25ddf5c8208f985c4eca65
edinstance <edinstance@gmail.com>
2025-10-13 18:03:55 +0100

    feat: setup config module for loading required config

--- services/gateway/src/main.rs
+++ services/gateway/src/main.rs
@@ -1,10 +1,17 @@
 mod verify;
+mod config;
 
 use axum::{Router, extract::Request, routing::any};
 use std::net::SocketAddr;
 
+use crate::config::Config;
+
 #[tokio::main]
 async fn main() {
+    let config = Config::from_env();
+    config.validate();
+    println!("Config is valid");
+
     let app = Router::new().route("/{*wildcard}", any(move |req| route_handler(req)));
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 1000));
